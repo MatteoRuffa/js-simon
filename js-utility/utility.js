f29bd25019dc49dead952362bedcb0532ac0bdaa
MatteoRuffa <cortese_17@protonmail.com>
2024-03-22 17:00:56 +0100

    add func.randomNumberUnique

--- js-utility/utility.js
+++ js-utility/utility.js
@@ -1,3 +1,17 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+// function generateUniqueRandomNumber(min, max, bombs) {
+//   let isFound = false;
+//   let randomNumber;
+                    
+//   while( !isFound) {
+//     randomNumber =  getRndInteger(min, max)
+                    
+//     if (bombs.includes(randomNumber) === false) {
+//       isFound = true;
+//     }
+//   }
+//   return randomNumber;
+// }
